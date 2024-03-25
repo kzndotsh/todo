98ed5126ef379ce178498020562fe804567d8318
kzndotsh <admin@kaizen.wtf>
2024-03-25 00:10:34 -0400

    [Feat] Add Express API server

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,17 +1,46 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import winston from "winston";
+import morgan from "morgan";
+
 import todoRouter from "./routes/todos";
 
 const app = express();
 const port = process.env.PORT ?? 3001;
 
-app.use(helmet()); // Security HTTP headers
-app.use(cors()); // Enable CORS
-app.use(express.json()); // Parse JSON body
+const logger = winston.createLogger({
+  level: "debug",
+  format: winston.format.json(),
+  defaultMeta: { service: "user-service" },
+  transports: [
+    new winston.transports.File({ filename: "error.log", level: "error" }),
+    new winston.transports.File({ filename: "combined.log" }),
+  ],
+});
+
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    }),
+  );
+}
+
+const winstonStream = {
+  write: (text: string) => {
+    logger.debug(text);
+  },
+};
+
+app.use(morgan("combined", { stream: winstonStream }));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello World!" });
+  res.json({ message: "API up" });
 });
 
 app.use("/api/todos", todoRouter);
